fab800096983546388d30be9b191887f6d11e30b
Luis <lucho2387@gmail.com>
2022-06-30 20:44:04 -0300

    make it better

--- src/server.js
+++ src/server.js
@@ -43,136 +43,118 @@ const { NAME, NAME_DATABASE, PASSWORD } = process.env
 
 // // Inicializacion
 const app = express()
+app.set('port', process.env.PORT || 8080)
 // require('./config/passport')
 
 // const PORT = parseInt(process.argv[2])
-const modoCluster = process.argv[3] == 'CLUSTER'
+// const modoCluster = process.argv[3] == 'CLUSTER'
 
+require('./config/passport')
 
+app.set("views", path.join(__dirname, "views"));
 
-app.set('port', process.env.PORT || 8080)
+// Motor de Plantilla (Handlebars)
+app.engine(
+    ".hbs",
+    handlebars.engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    })
+);
+app.set("view engine", ".hbs");
 
-if (modoCluster && cluster.isPrimary) {
- 
-    console.log(`Número de procesadores: ${numCPUs}`)
-    console.log(`PID MASTER ${process.pid}`)
- 
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork()
+// Middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'))
+app.use(cookieParser())
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: `mongodb+srv://${NAME}:${PASSWORD}@cluster0.9xnml.mongodb.net/${NAME_DATABASE}?retryWrites=true&w=majority`,
+        mongoOptions: advancedOptions
+    }),
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 6000000
     }
- 
-    cluster.on('exit', worker => {
-        console.log('Worker', worker.process.pid, 'died', new Date().toLocaleString())
-        cluster.fork()
-    })
- } else {
-    // Inicializacion
-    require('./config/passport')
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+// app.use(compression())
 
-    app.set("views", path.join(__dirname, "views"));
+
+// Variables Globales
+app.use((req, res, next) => {
+res.locals.mensaje = req.flash('mensaje')
+res.locals.error = req.flash('error')
+// res.locals.error = req.flash('error')
+res.locals.user = req.user || null;
+next()
+})
+
+app.get('/productos',isAuthenticated, async (req, res) => {
+    const user = req.user
+    const usuario = await Usuario.findById(user).lean()
+    const productos = await Producto.find().lean()
+    logger.info({ruta: req.url,metodo: req.method})
+    res.render('products/list-products', {usuario, productos})
     
-    // Motor de Plantilla (Handlebars)
-    app.engine(
-        ".hbs",
-        handlebars.engine({
-        defaultLayout: "main",
-        layoutsDir: path.join(app.get("views"), "layouts"),
-        partialsDir: path.join(app.get("views"), "partials"),
-        extname: ".hbs",
-        })
-    );
-    app.set("view engine", ".hbs");
+})
 
-    // Middlewares
-    app.use(morgan('dev'))
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: false }));
-    app.use(methodOverride('_method'))
-    app.use(cookieParser())
-    app.use(session({
-        store: MongoStore.create({
-            mongoUrl: `mongodb+srv://${NAME}:${PASSWORD}@cluster0.9xnml.mongodb.net/${NAME_DATABASE}?retryWrites=true&w=majority`,
-            mongoOptions: advancedOptions
-        }),
-        secret: 'secret',
-        resave: true,
-        saveUninitialized: true,
-        cookie: {
-            maxAge: 6000000
-        }
-    }))
-    app.use(passport.initialize())
-    app.use(passport.session())
-    app.use(flash())
-    // app.use(compression())
+app.get('/usuario/salir', async (req, res) => {
+    const user = req.user
+    const usuario = await Usuario.findById(user).lean()
+    req.logout()
+    req.flash('mensaje', `Hasta Luego ${usuario.nombre}`)
+    logger.info({ruta: req.url,metodo: req.method})
+    res.redirect('/usuario/login')
+})
 
 
-    // Variables Globales
-    app.use((req, res, next) => {
-    res.locals.mensaje = req.flash('mensaje')
-    res.locals.error = req.flash('error')
-    // res.locals.error = req.flash('error')
-    res.locals.user = req.user || null;
-    next()
-    })
+app.get('/info', async (req, res) => {
+    const util = require('util');
+    const directorio = process.cwd()
+    const ruta = process.execPath
+    const procesoId = process.pid
+    const nombrePlataforma = process.platform
+    const versionNode = process.version
+    const argumentoEntrada = process.argv
+    const memoriaReservada = util.inspect(process.memoryUsage().rss)
+    const totalCPUs = require('os').cpus().length
+    logger.info({ruta: req.url,metodo: req.method})
+    res.render('process/info',{directorio,ruta,procesoId,nombrePlataforma,versionNode,argumentoEntrada,memoriaReservada,totalCPUs})
+})
 
-    app.get('/productos',isAuthenticated, async (req, res) => {
-        const user = req.user
-        const usuario = await Usuario.findById(user).lean()
-        const productos = await Producto.find().lean()
-        logger.info({ruta: req.url,metodo: req.method})
-        res.render('products/list-products', {usuario, productos})
-        
-    })
-
-    app.get('/usuario/salir', async (req, res) => {
-        const user = req.user
-        const usuario = await Usuario.findById(user).lean()
-        req.logout()
-        req.flash('mensaje', `Hasta Luego ${usuario.nombre}`)
-        logger.info({ruta: req.url,metodo: req.method})
-        res.redirect('/usuario/login')
-    })
-
-
-    app.get('/info', async (req, res) => {
-        const util = require('util');
-        const directorio = process.cwd()
-        const ruta = process.execPath
-        const procesoId = process.pid
-        const nombrePlataforma = process.platform
-        const versionNode = process.version
-        const argumentoEntrada = process.argv
-        const memoriaReservada = util.inspect(process.memoryUsage().rss)
-        const totalCPUs = require('os').cpus().length
-        logger.info({ruta: req.url,metodo: req.method})
-        res.render('process/info',{directorio,ruta,procesoId,nombrePlataforma,versionNode,argumentoEntrada,memoriaReservada,totalCPUs})
-    })
-
-    // app.get("*", (req, res) => {
-    //     // console.log("REQ", req.url, req.method)
-    //     res.status(401).json({
-    //         descripcion:{
-    //                         ruta: `localhost:8080${req.originalUrl}`,
-    //                         metodo: req.method,
-    //                         mensaje: "Ruta No Encontrada",
-    //                         error: "401"
-    //                     }
-    //     });
-        
-    // })
+// app.get("*", (req, res) => {
+//     // console.log("REQ", req.url, req.method)
+//     res.status(401).json({
+//         descripcion:{
+//                         ruta: `localhost:8080${req.originalUrl}`,
+//                         metodo: req.method,
+//                         mensaje: "Ruta No Encontrada",
+//                         error: "401"
+//                     }
+//     });
+    
+// })
 
 
-    // Rutas
-    app.use(require('./routes/index.routes'))
-    app.use(require('./routes/productos.routes'))
-    app.use(require('./routes/usuarios.routes'))
-    app.use(require('./routes/random.routes'))
+// Rutas
+app.use(require('./routes/index.routes'))
+app.use(require('./routes/productos.routes'))
+app.use(require('./routes/usuarios.routes'))
+app.use(require('./routes/random.routes'))
 
-    // Archivos Estaticos
-    app.use(express.static(path.join(__dirname, "public")));
+// Archivos Estaticos
+app.use(express.static(path.join(__dirname, "public")));
 
- }
+ 
  app.listen(app.get('port'), () => {
     console.log('Servidor corriendo en el puerto:', app.get('port'))
 })

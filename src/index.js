351607cbb2ec8af5e37fcec1b8ffca1584399855
Luis <lucho2387@gmail.com>
2022-06-30 20:45:34 -0300

    make it better

--- src/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const { app } = require('./server')
-require('./database')
-
-
-app.listen(app.get('port'), () => {
-    console.log('Servidor corriendo en el puerto:', app.get('port'))
-})
